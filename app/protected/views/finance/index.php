96de51355d0a85123f99d78f5b2b2dfb078dc7f6
Aleksey Razbakov <aleksey@razbakov.com>
2011-12-22 18:43:49 +0200

    Finance Pockets

--- app/protected/views/finance/index.php
+++ app/protected/views/finance/index.php
@@ -5,3 +5,18 @@
         <li>Расходы</li>
     </ul>
 </div>
+<div id="pockets">
+    <h3>Кошельки</h3>
+    <div class="pocket">
+        <h4>Наличные Алексея</h4>
+        <blockquote>2151,00 грн</blockquote>
+    </div>
+    <div class="pocket">
+        <h4>Visa 2155</h4>
+        <blockquote>2151,00 грн</blockquote>
+    </div>
+    <div class="pocket">
+        <h4>MasterCard 2155</h4>
+        <blockquote>2151,00 грн</blockquote>
+    </div>
+</div>
--- app/protected/views/layouts/main.php
+++ app/protected/views/layouts/main.php
@@ -2,10 +2,10 @@
 <html>
 <head>
     <meta charset="utf-8"/>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700&subset=latin,cyrillic'
-          rel='stylesheet' type='text/css'>
+<!--    <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700&subset=latin,cyrillic'-->
+<!--          rel='stylesheet' type='text/css'>-->
     <link rel="stylesheet" href="<?php echo Yii::app()->request->baseUrl; ?>/css/style.css"/>
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
+<!--    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>-->
     <script>
         function getRandomInt(min, max) {
             return Math.floor(Math.random() * (max - min + 1)) + min;
